057b61e586db1028c93a227e7b79a5f649a5b733
Juan José Farina <juanjosefarina.jjf@gmail.com>
2023-08-02 20:09:54 -0300

    progressed

--- src/pages/MicroInteractions.js
+++ src/pages/MicroInteractions.js
@@ -6,22 +6,40 @@ import { faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import styles from './MicroInteractions.module.css';
 
+function rays () {
+  if(window.location.pathname === '/microInteractions') {
+    let background = document.getElementById('animatedBg');
+    let ray = document.createElement('div');
+    let wid = Math.random() * 20;
+    let heig = wid * 20;
+    let x = Math.random() * 90;
+    let y = Math.random() * 50;
+    ray.style.width = wid + 'px';
+    ray.style.height = heig + 'px';
+    ray.style.left = (x + 10) + 'vw';
+    ray.style.bottom = (y - 20) + 'vh';
+    ray.className = styles.backRay;
+    background.appendChild(ray);
+    setTimeout(() => {
+      background.removeChild(ray);
+    }, 20000);
+  }
+}
+
+setInterval(rays, 1000);
+
 export default function MicroInteractions() {
   const { language } = useContext(LanguageContext);
   return (
     <>
       <div className={styles.background} />
+      <div id='animatedBg' className={styles.animatedBg} />
       <div className="whitespace" />
       <div>
         <div className={styles.contenedor}>
-          <div className={styles.hero} />
-          <h1 className={styles.title}>Juan José Farina</h1>
-          <h2 className={styles.subtitle}>{text[language]['title']}</h2>
+          <h1 className={styles.name}>Juan José Farina</h1>
+          <h2 className={styles.title}>{text[language]['title']}</h2>
           <div className={styles.arrows}>
-            <svg className={styles.arrowDown} width="100px" height="100px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M 12 0 L 12 24" stroke="black" fill="none" />
-              <path d="M 12 24 L 6 16 M 12 24 L 18 16" stroke="black" fill="none" />
-            </svg>
             <svg className={styles.arrowDown} width="100px" height="100px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M 12 0 L 12 24" stroke="black" fill="none" />
               <path d="M 12 24 L 6 16 M 12 24 L 18 16" stroke="black" fill="none" />
